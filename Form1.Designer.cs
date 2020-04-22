5a6373a7fdac764583e52902e4d2903e4920a191
Eugene <chemodanik807@gmail.com>
2020-04-22 13:46:39 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TalkBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.ListMessages = new System.Windows.Forms.ListBox();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(133, 18);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(105, 22);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 21);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(115, 17);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Никнейм в сети:";
+            // 
+            // ListMessages
+            // 
+            this.ListMessages.FormattingEnabled = true;
+            this.ListMessages.ItemHeight = 16;
+            this.ListMessages.Location = new System.Drawing.Point(12, 57);
+            this.ListMessages.Name = "ListMessages";
+            this.ListMessages.Size = new System.Drawing.Size(776, 276);
+            this.ListMessages.TabIndex = 2;
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Location = new System.Drawing.Point(12, 348);
+            this.textBoxMsg.Multiline = true;
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(776, 51);
+            this.textBoxMsg.TabIndex = 3;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSend.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSend.Location = new System.Drawing.Point(665, 405);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(123, 33);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = false;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonConnect.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.Location = new System.Drawing.Point(628, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(160, 33);
+            this.buttonConnect.TabIndex = 5;
+            this.buttonConnect.Text = "Подключиться";
+            this.buttonConnect.UseVisualStyleBackColor = false;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.ListMessages);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TextBox";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ListBox ListMessages;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonConnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TalkBox
+{
+    public partial class Form1 : Form
+    {
+        String UserName;
+        internal bool Connected = false;
+        
+
+
+        internal class TalkPacket
+        {
+            public byte Type;
+            public UInt16 MsgLength;
+            public byte[] Data;
+
+            public TalkPacket(byte T, string D)
+            {
+                Type = T;
+                Data = D.ToByteArr();
+                MsgLength = (UInt16)(3 + Data.Length);
+            }
+            public TalkPacket(byte T)
+            {
+                Type = T;
+                MsgLength = 3;
+            }
+            public TalkPacket(byte[] D)
+            {
+                Type = D[0];
+                MsgLength = BitConverter.ToUInt16(D, 1);
+                if (MsgLength > 3)
+                {
+                    Data = new byte[MsgLength-3];
+                    Buffer.BlockCopy(D, 3, Data, 0, MsgLength - 3);
+                }
+            }
+            public byte[] getBytes()
+            {
+                byte[] data = new byte[MsgLength];
+                Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+                Buffer.BlockCopy(BitConverter.GetBytes(MsgLength), 0, data, 1, 2);
+                if (MsgLength > 3)
+                    Buffer.BlockCopy(Data, 0, data, 3, MsgLength-3);
+                return data;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (buttonConnect.Text == "Подключиться")
+            {
+                UserName = textBoxName.Text.Trim();
+                if (UserName.Length > 3 && UserName.Length < 13)
+                {
+                    ConnectChat();
+                }
+                else
+                    MessageBox.Show("Введите имя для вашего аккаунта!");
+            }
+            else
+            {
+                LeaveChat();
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (Connected)
+            {
+                UdpClient client = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 8005);
+                TalkPacket msg = new TalkPacket(3, textBoxMsg.Text);
+                textBoxMsg.Text = "";
+                client.Send(msg.getBytes(), msg.MsgLength,endPoint);
+                client.Close();
+            }
+            else
+                MessageBox.Show("Вы не подключены к чату.");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Connected)
+                LeaveChat();
+        }
+        private void ConnectChat()
+        {
+            textBoxName.Text = UserName;
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 8005);
+            try
+            {
+                TalkPacket msg = new TalkPacket(1, UserName);
+                client.Send(msg.getBytes(), msg.MsgLength, endPoint);
+                client.Close();
+                textBoxName.Enabled = false;
+                buttonConnect.Text = "Отключиться";
+                Connected = true;
+                Task receiveTask = new Task(ReceiveMessage);
+                receiveTask.Start();
+                MessageBox.Show("Вы подключились к чату");
+            }
+            catch (Exception ex)
+            {
+                client.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void LeaveChat()
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("230.230.230.230"), 8005);
+            try
+            {
+                TalkPacket msg = new TalkPacket(0);
+                client.Send(msg.getBytes(), msg.MsgLength, endPoint);
+                textBoxName.Enabled = true;
+                buttonConnect.Text = "Подключиться";
+                Connected = false;
+                MessageBox.Show("Вы отключились от чата");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+        private void ReceiveMessage()
+        {
+            UdpClient client = new UdpClient(8005);
+            client.JoinMulticastGroup(IPAddress.Parse("230.230.230.230"), 50);
+            IPEndPoint remoteIp = null;
+            try
+            {
+                while (Connected)
+                {
+                    byte[] data = client.Receive(ref remoteIp);
+                    TalkPacket msg = new TalkPacket(data);
+                    // Отображение полученного сообщения
+                    // ListMessages.Items.Add(Encoding.ASCII.GetString(msg.Data));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+    public static class StringExtension
+    {
+        public static byte[] ToByteArr(this string str)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(str);
+            return data;
+        }
+    }
+}
